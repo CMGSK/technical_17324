623da7c25d76aeb27da779bc001ecf504c1ae266
CMGSK@MSI <carlosmgov7k@gmail.com>
2024-05-20 20:04:01 +0200

    all logic implemented (git -am flag doesnt work in VSC im so mad about it)

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { Data, Router } from '@angular/router';
+import { DataService } from './login.dataService';
+
+// La hardcodeamos porque en este caso no la vamos a usar mucho.
+// En otro caso tendriamos que enviarla por HTTPS al backend para que se 
+// valide correctamente contra su hash
+const pwd = "veosat";
+
+@Component({
+  selector: 'app-login',
+  providers: [DataService],
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  // Como es solo una prueba para comunicar componentes, lo estoy haciendo de una forma rapida,
+  // idealmente esto tendria que pasar por OAuth, tendriamos que redirigir a home cuando
+  // tenemos el token de login y evitar que se pueda cargar esta pagina, etc. y lo hariamos 
+  // con un form de autenticacion de la libreria de primeng
+
+  user: string;
+  fail: boolean;
+
+  constructor(private router: Router, private ds: DataService) { }
+
+  ngOnInit(): void { }
+
+  validate(user:string, password:string) {
+    //Si la contrasena valida, redirigimos al home guardandonos el username
+    if (password == pwd) {
+      this.user = user;
+      this.ds.dataIn(user);
+      this.router.navigate(['/home']);
+    }
+    else this.fail = true;
+  }
+
+}
---
+++ src/app/login/login.dataService.ts
@@ -0,0 +1,22 @@
+import { Injectable } from "@angular/core";
+
+//Como no he utilizado parenting, comunicamos de forma sencilla los datos a traves de 
+//este servicio. Con parenting, podria utilizar @Input.
+
+@Injectable()
+export class DataService {
+
+    private msg: string;
+
+    constructor() { }
+
+    //Asignamos datos
+    dataIn(msg: string) {
+        this.msg = msg;
+    }
+
+    //Recogemos datos
+    dataOut(): string {
+        return this.msg;
+    }
+}
